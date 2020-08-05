f0895bd2fe56b8591da7e5bfc781c9cb5270548e
John Vandenberg <jayvdb@gmail.com>
2020-08-05 17:13:01 +0700

    setup.py: Fix name

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dheavy", version="1.0.0", packages=find_packages(), python_requires=">=3.7",
+    name="djheavy", version="1.0.0", packages=find_packages(), python_requires=">=3.7",
 )
